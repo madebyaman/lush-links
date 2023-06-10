170ba4bd46c53c0dc3b8fd5a5d82fe456e0bf84f
Aman Thakur <builtbyaman@gmail.com>
2023-06-11 03:13:53 +0530

    format private key

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -4,7 +4,9 @@ if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
+        : undefined,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     }),
     databaseURL: process.env.FIREBASE_DB_URL
